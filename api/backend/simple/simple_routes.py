cb1bdd410020a972638023e510585fe39540d26d
carolineamonaco <carolineamonaco@gmail.com>
2024-11-14 14:09:09 -0500

    test

--- api/backend/simple/simple_routes.py
+++ api/backend/simple/simple_routes.py
@@ -4,6 +4,7 @@ from backend.db_connection import db
 from backend.simple.playlist import sample_playlist_data
 
 # This blueprint handles some basic routes that you can use for testing
+    # collection of routes in flask - group of routes w common name
 simple_routes = Blueprint('simple_routes', __name__)
 
 
