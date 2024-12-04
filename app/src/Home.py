e816ce04902e1ae54d04a64bc447955787737a90
carolineamonaco <carolineamonaco@gmail.com>
2024-12-04 14:35:24 -0500

    Update comments

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,88 +1,77 @@
-##################################################
-# This is the main/entry-point file for the 
-# sample application for your project
-##################################################
-
-# Set up basic logging infrastructure
+# Establish basic logging infrastructure
 import logging
 logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
+# Imports
 import streamlit as st
 from modules.nav import SideBarLinks
 
+# Set default session state values
 st.session_state['authenticated'] = False
 st.session_state['user_id'] = None
 st.session_state['first_name'] = None
 st.session_state['coordinator_id'] = None
 st.session_state['admin_id'] = None
 
-# streamlit supports reguarl and wide layout (how the controls
-# are organized/displayed on the screen).
+# Make wide layout
 st.set_page_config(layout = 'wide')
 
-# If a user is at this page, we assume they are not 
-# authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false. 
-st.session_state['authenticated'] = False
-
-# Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel. 
-# IMPORTANT: ensure src/.streamlit/config.toml sets
-# showSidebarNavigation = false in the [client] section
+# Links on side bar
 SideBarLinks(show_home=True)
 
-# ***************************************************
-#    The major content of this page
-# ***************************************************
-
-# set the title of the page and provide a simple prompt. 
+# Create title and insert slogan and prompt
 logger.info("Loading the Home page of the app")
 st.title('PeerPath')
 st.write('An unfiltered network for professional growth.')
 st.write('### Select a user to log in as:')
 
-# For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user 
-# can click to MIMIC logging in as that mock user. 
-
+# Create buttons for each persona that mimic logging in
 if st.button("Act as Jerry, a 2nd year student searching for his 1st co-op", 
             type = 'primary', 
             use_container_width=True):
-    # when user clicks the button, they are now considered authenticated
+    # Change authentication to True (user is now "logged in")
     st.session_state['authenticated'] = True
-    # we set the user id of the current user
+    # Set id of current user (Jerry)
     st.session_state['user_id'] = 1
-    # we add the first name of the user (so it can be displayed on 
-    # subsequent pages). 
+    # Add first name of user
     st.session_state['first_name'] = 'Jerry'
-    # finally, we ask streamlit to switch to another page, in this case, the 
-    # landing page for this particular user type
+    # Switch to Jerry's home page
     logger.info("Logging in as Jerry (First Time Co-op Persona)")
     st.switch_page('pages/Jerry_Home.py')
 
 if st.button('Act as Jordan, a 4th year finishing his second co-op', 
             type = 'primary', 
             use_container_width=True):
+    # Change authentication to True (user is now "logged in")
     st.session_state['authenticated'] = True
+    # Set id of current user (Jordan)
     st.session_state['user_id'] = 2
+    # Add first name of user
     st.session_state['first_name'] = 'Jordan'
+    # Switch to Jordan's home page
     st.switch_page('pages/Jordan_Home.py')
 
 if st.button('Act as Stacy, a co-op coordinator', 
             type = 'primary', 
             use_container_width=True):
+    # Change authentication to True (user is now "logged in")
     st.session_state['authenticated'] = True
+    # Set id of current coordinator (Stacy)
     st.session_state['coordinator_id'] = 1
+    # Add first name of user
     st.session_state['first_name'] = 'Stacy'
+    # Switch to Stacy's home page
     st.switch_page('pages/Stacy_Home.py')
 
 if st.button('Act as Phil, a system administrator', 
             type = 'primary', 
             use_container_width=True):
+    # Change authentication to True (user is now "logged in")
     st.session_state['authenticated'] = True
+    # Set id of current system admin (Phil)
     st.session_state['admin_id'] = 1
+    # Add first name of user
     st.session_state['first_name'] = 'Phil'
+    # Switch to Phil's home page
     st.switch_page('pages/Phil_Home.py')
