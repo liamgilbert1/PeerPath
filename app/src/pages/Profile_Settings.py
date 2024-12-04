fac910966ba75954e50d1fbc7b247a412ed94725
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-12-04 15:23:15 -0500

    Activity Status and Urgency Status Dropdowns Added

--- app/src/pages/Profile_Settings.py
+++ app/src/pages/Profile_Settings.py
@@ -28,13 +28,15 @@ current_activity_status = [item.get("activity_status", "") for item in data][0]
 current_urgency_status = [item.get("urgency_status", "") for item in data][0]
 current_search_status = [item.get("search_status", "") for item in data][0]
 
+activity_status_options = ["offline", "online"]
+urgency_status_options = ["not urgent", "semi-urgent", "urgent"]
 search_status_options = ["applying", "job found", "future applicant", "next cycle applicant", "on-coop"]
 
 # Add a text input field for the user to input their first name
 username = st.text_input("Username:", value=current_username)
 email = st.text_input("Email:", value=current_email)
-activity_status = st.text_input("Activity Status:", value=current_activity_status)
-urgency_status = st.text_input("Urgency Status:", value=current_urgency_status)
+activity_status = st.selectbox("Activity Status:", activity_status_options, activity_status_options.index(current_activity_status) if activity_status_options in activity_status_options else 0)
+urgency_status = st.selectbox("Urgency Status:", urgency_status_options, urgency_status_options.index(current_urgency_status) if urgency_status_options in urgency_status_options else 0)
 search_status = st.selectbox("Search Status:", search_status_options, search_status_options.index(current_search_status) if current_search_status in search_status_options else 0)
 
 def update_profile(username, email, activity_status, urgency_status, search_status):
