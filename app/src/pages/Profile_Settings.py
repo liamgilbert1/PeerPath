e0a01b18918f83a81777a0b275d5aac8d550527c
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-12-04 15:17:35 -0500

    Profile Settings Input Fields Added

--- app/src/pages/Profile_Settings.py
+++ app/src/pages/Profile_Settings.py
@@ -25,3 +25,12 @@ current_email = [item.get("email", "") for item in data][0]
 current_activity_status = [item.get("activity_status", "") for item in data][0]
 current_urgency_status = [item.get("urgency_status", "") for item in data][0]
 current_search_status = [item.get("search_status", "") for item in data][0]
+
+search_status_options = ["applying", "job found", "future applicant", "next cycle applicant", "on-coop"]
+
+# Add a text input field for the user to input their first name
+username = st.text_input("Username:", value=current_username)
+email = st.text_input("Email:", value=current_email)
+activity_status = st.text_input("Activity Status:", value=current_activity_status)
+urgency_status = st.text_input("Urgency Status:", value=current_urgency_status)
+search_status = st.selectbox("Search Status:", search_status_options, search_status_options.index(current_search_status) if current_search_status in search_status_options else 0)
