d294d4b511a8b2b5c3468e9033d01cc68862acae
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-12-04 01:42:39 -0500

    example blueprint and route added for persona2

---
+++ api/backend/persona2/persona2_routes.py
@@ -0,0 +1,32 @@
+########################################################
+# Sample customers blueprint of endpoints
+# Remove this file if you are not using it in your project
+########################################################
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+#------------------------------------------------------------
+# Create a new Blueprint object, which is a collection of 
+# routes.
+persona2 = Blueprint('persona2', __name__)
+
+
+#------------------------------------------------------------
+# Get specific user from the system
+@persona2.route('/user/<int:userID>', methods=['GET'])
+def get_user(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM user WHERE user_id = %s', (userID,))
+    
+    theData = cursor.fetchall()
+    
+    if not theData:
+        return make_response(jsonify({"error": "RIP"}), 404)
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
