1efeb2e5b6155fb6edde041d7ade6fbba67b21e2
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-12-04 15:16:33 -0500

    Profile Settings Query Implemented

--- app/src/pages/Profile_Settings.py
+++ app/src/pages/Profile_Settings.py
@@ -12,3 +12,10 @@ st.write("# Profile Settings")
 """
 Use this page to update your profile settings. Make your changes, then click the 'Save' button to update your profile.
 """
+
+data = {} 
+try:
+  data = requests.get(f'http://api:4000/c/user/{user_id}').json()
+except:
+  st.write("**Important**: Could not connect to sample api, so using dummy data.")
+  data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
