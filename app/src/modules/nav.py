9fdd6548e5f9fa484a013ac4fa7eaccd5bab2184
carolineamonaco <carolineamonaco@gmail.com>
2024-12-04 13:21:22 -0500

    Updated navigator to be based on user_id rather than role

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -78,7 +78,7 @@ def SideBarLinks(show_home=False):
     if st.session_state["authenticated"]:
 
         # Show World Bank Link and Map Demo Link if the user is a political strategy advisor role.
-        if st.session_state["role"] == "pol_strat_advisor":
+        if st.session_state["user_id"] == 1:
             PolStratAdvHomeNav()
             WorldBankVizNav()
             MapDemoNav()
