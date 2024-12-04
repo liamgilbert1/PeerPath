1e633a3a33dd8f096cef277946c4dcf03fc957a1
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-12-04 01:31:55 -0500

    Example streamlist API test added for persona1 example route

--- app/src/pages/12_API_Test.py
+++ app/src/pages/12_API_Test.py
@@ -17,7 +17,8 @@ should not totally die.
 """
 data = {} 
 try:
-  data = requests.get('http://api:4000/p/products').json()
+  user_id = 3
+  data = requests.get(f'http://api:4000/c/user/{user_id}').json()
 except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
