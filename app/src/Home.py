fd2c5d001b9c6d2e63e53b6c5f35e730664e415c
carolineamonaco <carolineamonaco@gmail.com>
2024-12-04 13:17:11 -0500

    Update button for persona 1 (Jerry)

--- app/src/Home.py
+++ app/src/Home.py
@@ -35,26 +35,26 @@ SideBarLinks(show_home=True)
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
 st.title('PeerPath')
-st.write('\n\n')
-st.write('### HI! As which user would you like to log in?')
+st.write('An unfiltered network for professional growth.')
+st.write('### Select a user to log in as:')
 
 # For each of the user personas for which we are implementing
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
-if st.button("Act as John, a Political Strategy Advisor", 
+if st.button("Act as Jerry, a 2nd year student searching for his 1st co-op", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
-    # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    # we set the user id of the current user
+    st.session_state['user_id'] = 1
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
     st.session_state['first_name'] = 'John'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
+    logger.info("Logging in as Jerry (First Time Co-op Persona)")
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
 if st.button('Act as Mohammad, an USAID worker', 
