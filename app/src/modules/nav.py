35f05ec97ff812146d32026b48cfb6c32479886f
carolineamonaco <carolineamonaco@gmail.com>
2024-12-04 14:41:31 -0500

    Update Phil home page name in nav

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -50,7 +50,7 @@ def ClassificationNav():
 
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
-    st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
+    st.sidebar.page_link("pages/Phil_Home.py", label="Phil's Home Page", icon="🖥️")
     st.sidebar.page_link(
         "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
     )
