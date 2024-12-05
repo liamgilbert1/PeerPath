49b4a88d40f85a9026fdc2dbd343593491a0ad68
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-12-05 00:09:56 -0500

    View, add, and remove friend page buttons added to persona1 home page

--- app/src/pages/Jerry_Home.py
+++ app/src/pages/Jerry_Home.py
@@ -9,7 +9,7 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome, {st.session_state['first_name']}.")
+st.title(f"Welcome, {st.session_state['first_name']}!")
 st.write('')
 st.write('')
 st.write('### How can we help you on your co-op search?')
@@ -28,3 +28,18 @@ if st.button('Find Resources',
             type='primary',
             use_container_width=True):
   st.switch_page('pages/Find_Resources.py')
+
+if st.button('Add Friends', 
+            type='primary',
+            use_container_width=True):
+  st.switch_page('pages/Add_Friend.py')
+
+if st.button('Remove Friends', 
+            type='primary',
+            use_container_width=True):
+  st.switch_page('pages/Remove_Friend.py')
+
+if st.button('Profile Settings', 
+            type='primary',
+            use_container_width=True):
+  st.switch_page('pages/Profile_Settings.py')
