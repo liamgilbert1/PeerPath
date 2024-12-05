58346f852b062cb3a924f6b8490e5aa9be1c9df5
Liam Gilbert <gilbert.li@northeastern.edu>
2024-12-05 13:34:55 -0500

    Made Stacy's home page for persona3 cleaner and added descriptions over each button

--- app/src/pages/Stacy_Home.py
+++ app/src/pages/Stacy_Home.py
@@ -14,16 +14,28 @@ st.write('')
 st.write('')
 st.write('### What would you like to do?')
 
+st.write('')
+st.write('')
+
+st.write('##### Click here to view all users and find a user based on their username.')
 if st.button("Find and View Users",
              type='primary',
              use_container_width=True):
   st.switch_page('pages/Users.py')
 
+st.write('')
+st.write('')
+
+st.write('##### Click here to view all ratings and find ratings for specific coordinators.')
 if st.button("Retrieve Ratings",
              type='primary',
              use_container_width=True):
   st.switch_page('pages/Ratings.py')
 
+st.write('')
+st.write('')
+
+st.write('##### Click here to view, create, update, and delete notes.')
 if st.button("Manage Notes",
              type='primary',
              use_container_width=True):
