224e47e8b93923a502e76746ea36e5d0a434a883
Liam Gilbert <gilbert.li@northeastern.edu>
2024-12-05 22:21:44 -0500

    Persona4 pages numbered

--- app/src/pages/22_Phil_Home.py
+++ app/src/pages/22_Phil_Home.py
@@ -19,12 +19,12 @@ st.subheader("Activity and Alerts")
 col1, col2 = st.columns(2)
 with col1:
     if st.button('View Hourly Activity', type='primary', use_container_width=True):
-        st.switch_page('pages/View_Activity.py')
+        st.switch_page('pages/23_View_Activity.py')
     if st.button('View All System Alerts', type='primary', use_container_width=True):
-        st.switch_page('pages/View_Alerts.py')
+        st.switch_page('pages/24_View_Alerts.py')
 with col2:
     if st.button('Create New Alert', type='primary', use_container_width=True):
-        st.switch_page('pages/Add_Alert.py')
+        st.switch_page('pages/25_Add_Alert.py')
 
 st.write('')
 
@@ -33,11 +33,11 @@ st.subheader("User Management")
 col3, col4 = st.columns(2)
 with col3:
     if st.button('Manage User Permissions', type='primary', use_container_width=True):
-        st.switch_page('pages/User_Permissions.py')
+        st.switch_page('pages/26_User_Permissions.py')
     if st.button('Manage Users', type='primary', use_container_width=True):
-        st.switch_page('pages/Manage_Users.py')
+        st.switch_page('pages/27_Manage_Users.py')
 with col4:
     if st.button('Manage Advice', type='primary', use_container_width=True):
-        st.switch_page('pages/Manage_Advice.py')
+        st.switch_page('pages/28_Manage_Advice.py')
     if st.button('Manage Requests', type='primary', use_container_width=True):
-        st.switch_page('pages/Help_Requests.py')
+        st.switch_page('pages/29_Help_Requests.py')
